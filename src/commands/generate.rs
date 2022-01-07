9e246aa04ba7957e3d475e09315b6d35a05253c8
Kevin Rodriguez <_@kevinrodriguez.io>
2022-01-07 12:05:02 -0600

    feat: almost magic

--- src/commands/generate.rs
+++ src/commands/generate.rs
@@ -1,6 +1,6 @@
 use crate::state::{
     layers_config::{LayerConfiguration, LayerItem, PickedLayerItem},
-    nft_metadata::Metadata,
+    nft_metadata::{Attribute, Metadata},
 };
 use crate::tools::mkdirp::mkdirp;
 use anyhow::Result;
@@ -9,8 +9,8 @@ use image::imageops;
 use rand::distributions::WeightedIndex;
 use rand::prelude::*;
 use serde_json::from_str;
-use std::{borrow::Borrow, collections::HashMap};
-use std::{error::Error, path::Path};
+use std::collections::HashMap;
+use std::path::Path;
 use std::{fs, path};
 
 fn read_json_template(json_template_path: &String) -> Result<Metadata> {
@@ -67,16 +67,15 @@ fn build_image(
     output_path: &String,
     file_name_no_ext: &String,
     items: Vec<LayerItem>,
-) -> Result<()> {
+) -> Result<String> {
     let result = match items.len() {
         0 => Err(anyhow::anyhow!("No layers to build image from")),
         1 => {
             let item = &items[0];
-            image::open(path::Path::new(&item.uri))?.save_with_format(
-                output_path.to_owned() + "/" + file_name_no_ext + ".jpeg",
-                image::ImageFormat::Jpeg,
-            )?;
-            Ok(())
+            let path = output_path.to_owned() + "/" + file_name_no_ext + ".jpeg";
+            image::open(path::Path::new(&item.uri))?
+                .save_with_format(path.clone(), image::ImageFormat::Jpeg)?;
+            Ok(path)
         }
         _ => {
             let mut base_image = image::open(path::Path::new(&items[0].uri))?;
@@ -84,23 +83,53 @@ fn build_image(
                 let current_layer = image::open(path::Path::new(&items[i].uri))?;
                 imageops::overlay(&mut base_image, &current_layer, 0, 0);
             }
-            base_image.save_with_format(
-                output_path.to_owned() + "/" + file_name_no_ext + ".jpeg",
-                image::ImageFormat::Jpeg,
-            )?;
-            Ok(())
+            let path = output_path.to_owned() + "/" + file_name_no_ext + ".jpeg";
+            base_image.save_with_format(path.clone(), image::ImageFormat::Jpeg)?;
+            Ok(path)
         }
     };
-    return result;
+    result
 }
 
-fn build_json(output_path: &String, file_name_no_ext: &String, json_template: &Metadata) {
+fn build_json(
+    output_path: &String,
+    file_name_no_ext: &String,
+    json_template: &Metadata,
+    items: Vec<LayerItem>,
+) -> Result<(Metadata, String)> {
     let mut output_json = json_template.clone();
-    // output_json.id = file_name_no_ext.to_owned();
-    // output_json.uri = output_path.to_owned() + "/" + file_name_no_ext + ".jpeg";
-    let output_json_str = serde_json::to_string(&output_json).unwrap();
+    output_json.name = output_json.name + " - " + format!("{:0>4}", file_name_no_ext).as_str();
+    output_json.attributes = items
+        .into_iter()
+        .map(|item| Attribute {
+            trait_type: item.layer_name,
+            value: item.item,
+        })
+        .collect();
+    output_json.image = file_name_no_ext.to_owned() + ".jpeg";
+    let output_json_str = serde_json::to_string(&output_json)?;
     let output_json_path = output_path.to_owned() + "/" + file_name_no_ext + ".json";
-    fs::write(output_json_path, output_json_str).unwrap();
+    fs::write(output_json_path.clone(), output_json_str)?;
+    Ok((output_json, output_json_path))
+}
+
+fn generate_item(
+    layers_config: &HashMap<String, LayerConfiguration>,
+    layers_path: &String,
+    file_name_no_ext: &String,
+    output_path: &String,
+    json_template: &Metadata,
+) -> Result<(Metadata, String, String)> {
+    let picked_layers = pick_layers(layers_config)?;
+    let picked_layer_items = get_picked_layer_item_uris(layers_path, picked_layers);
+    let image_path = build_image(output_path, &file_name_no_ext, picked_layer_items.clone())?;
+    let (metadata, json_path) = build_json(
+        output_path,
+        &file_name_no_ext,
+        json_template,
+        picked_layer_items,
+    )?;
+    Ok((metadata, image_path, json_path))
 }
 
 pub fn exec(
@@ -124,15 +153,15 @@ pub fn exec(
         _ => (),
     };
     for i in 0..*amount {
-        let picked_layers = match pick_layers(&layers_config) {
-            Ok(result) => result,
-            Err(e) => panic!("Error picking layers: {}", e),
-        };
-        let picked_layer_items = get_picked_layer_item_uris(layers_path, picked_layers);
-        let file_name_no_ext = i.to_string();
-        match build_image(output_path, &file_name_no_ext, picked_layer_items) {
-            Err(e) => panic!("Error building image: {}", e),
+        match generate_item(
+            &layers_config,
+            layers_path,
+            &i.to_string(),
+            output_path,
+            &json_template,
+        ) {
+            Err(e) => panic!("Error building item {}: {}", i, e),
             _ => (),
-        };
+        }
     }
 }
